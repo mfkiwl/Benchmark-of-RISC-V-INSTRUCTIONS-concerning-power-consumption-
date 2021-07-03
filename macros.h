a0d8d7fd33fc60484a060b4f99a176b206fac620
Vicky <79350667+Vicky-ICS@users.noreply.github.com>
2021-07-03 23:22:44 +0530

    Add files via upload

---
+++ macros.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2013 Bluespec, Inc.  All Rights Reserved
+
+// ----------------------------------------------------------------
+// Pass/Fail macros. This is a temporary place-holder. To be moved to an
+// appropriate location under the env directory structure once we can converge
+// on a unified build environment for all tests
+#define TEST_PASS li x28, 0x1
+#define TEST_FAIL li x28, 0x3
+//#define clsize 64
